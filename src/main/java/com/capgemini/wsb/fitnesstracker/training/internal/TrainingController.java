0736c32f4478e3ed8d1d3f418cba698729350871
Adam Burski <burskiadamwork@gmail.com>
2024-05-30 12:30:16 +0200

    Repair Training controller

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
@@ -4,17 +4,19 @@ import com.capgemini.wsb.fitnesstracker.training.api.ITrainingProvider;
 import com.capgemini.wsb.fitnesstracker.training.api.ITrainingService;
 import com.capgemini.wsb.fitnesstracker.training.api.TrainingDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/training")
+@RequestMapping("/v1/trainings")
 @RequiredArgsConstructor
 public class TrainingController {
-    public ITrainingProvider trainingProvider;
-    public ITrainingService trainingService;
+
+    private final ITrainingProvider trainingProvider;
+    private final ITrainingService trainingService;
 
     @GetMapping("{id}")
     public TrainingDto getTraining(@PathVariable("id") Long id)
