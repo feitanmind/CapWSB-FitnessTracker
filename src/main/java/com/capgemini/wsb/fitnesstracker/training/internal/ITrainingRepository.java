67999d5c7cb7a6353b8ed270470a966af2a7eb58
Adam Burski <burskiadamwork@gmail.com>
2024-05-30 13:59:54 +0200

    Create repository methods to return list of trainings with specified filter

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/ITrainingRepository.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/ITrainingRepository.java
@@ -3,14 +3,59 @@ package com.capgemini.wsb.fitnesstracker.training.internal;
 import com.capgemini.wsb.fitnesstracker.training.api.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.*;
 
 interface ITrainingRepository extends JpaRepository<Training, Long> {
-
+    /**
+     * Find all trainings for user
+     * @param userId
+     * @return List(Training)
+     */
     default List<Training> findAllTrainingsForUser(Long userId)
     {
         return findAll().stream().filter(t -> Objects.equals(t.getUser().getId(), userId)).toList();
     }
+
+    /**
+     * Return list of trainings for user with end date before specified date
+     * @param date
+     * @param userId
+     * @return List(Training)
+     */
+    default List<Training> findTrainingsWithEndDateBeforeSpecifiedEndDate(Date date, Long userId)
+    {
+        return findAllTrainingsForUser(userId).stream().filter(t -> t.getEndTime().before(date)).toList();
+    }
+    /**
+     * Return list of trainings with end date before specified date for all users
+     * @param date
+     * @return List(Training)
+     */
+    default List<Training> findTrainingsWithEndDateBeforeSpecifiedEndDate(Date date)
+    {
+        return findAll().stream().filter(t -> t.getEndTime().before(date)).toList();
+    }
+
+    /**
+     * Return list of trainings for user with specified activityTYpe
+     * @param activityType
+     * @param userId
+     * @return
+     */
+    default List<Training> findAllTrainingForSpecifiedActivity(ActivityType activityType, Long userId)
+    {
+        return findAllTrainingsForUser(userId).stream().filter(t -> t.getActivityType() == activityType).toList();
+    }
+
+    /**
+     * Return list of trainings for all users with specified activityTYpe
+     * @param activityType
+     * @return
+     */
+    default List<Training> findAllTrainingForSpecifiedActivity(ActivityType activityType)
+    {
+        return findAll().stream().filter(t -> t.getActivityType() == activityType).toList();
+    }
+
 }
